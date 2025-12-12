72f330951fead32298a78ed74fd7be66235dc522
SaschaRunge <sascharunge89@yahoo.de>
2025-12-12 09:06:17 +0100

    implemented parent nodes, updated corresponding tests

--- htmlnode.py
+++ htmlnode.py
@@ -10,8 +10,9 @@ class HTMLNode():
     
     def props_to_html(self):
         html_string = ""
-        for key, value in self.props.items():
-            html_string += f" {key}=\"{value}\""
+        if self.props:
+            for key, value in self.props.items():
+                html_string += f" {key}=\"{value}\""
         return html_string
     
     def __repr__(self):
@@ -19,20 +20,30 @@ class HTMLNode():
     
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        if not value:
+        if value is None:
             raise ValueError(f"Leaf node {self} has no value.")
         super().__init__(tag, value, None, props)
     
     def to_html(self):
-        if not self.value:
+        if self.value is None:
             raise ValueError(f"Leaf node {self} has no value.")
         if not self.tag:
-            return self.value
-        
-        if not self.props:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-                
+            return self.value          
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError(f"Parent node {self} has no tag.")
+        if not self.children:
+            raise ValueError(f"Parent node {self} has no child(ren).")
+        html_string = ""
+        for child in self.children:
+            html_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
 
 
         
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -51,12 +51,94 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_value_error(self):
         def _helper():
             node = LeafNode("p", "this is another test")
-            node.value = ""
+            node.value = None
             node.to_html()
         self.assertRaises(ValueError, _helper)
 
-  
+    #probably not a correct way to handle <img> -> See void elements
+    def test_leaf_empty_string_value(self):
+        node = LeafNode("img", "")
+        self.assertEqual(node.to_html(), "<img></img>")
+    
+    def test_leaf_no_tag(self):
+        node = LeafNode(None, "this is yet another test")
+        self.assertEqual(node.to_html(), "this is yet another test")
 
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+    
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+    
+    # <img> and <br> tags are handled like any other tag for now, likely not correct html but it will do as a test for now
+    def test_to_html_with_grandchildren_and_props(self):
+        grandchild_node = LeafNode("br", "")
+        child_node = ParentNode("img", [grandchild_node], {
+            "src": "smiley.gif",
+            "alt": "HTML tutorial", 
+            "style": "width:42px;height:42px;"
+        })
+        parent_node = ParentNode("a", [child_node], {"href": "default.asp"})
+        self.assertEqual(
+            parent_node.to_html(),
+            '<a href="default.asp"><img src="smiley.gif" alt="HTML tutorial" style="width:42px;height:42px;"><br></br></img></a>',
+        )
+
+    def test_to_html_with_multiple_children(self):
+        child_node1 = LeafNode("b", "i am bold")
+        child_node2 = LeafNode("i", "i am italic")
+        child_node3 = LeafNode("blockquote", "i am a quote")
+        children = [child_node1, child_node2, child_node3]
+        parent_node = ParentNode("p", children)
+        self.assertEqual(
+            parent_node.to_html(),
+            "<p><b>i am bold</b><i>i am italic</i><blockquote>i am a quote</blockquote></p>",
+        )
+
+    def test_to_html_with_multiple_children_and_grandchildren(self):
+        children1 = [LeafNode("b", "i am bold"), LeafNode("i", "i am italic"), LeafNode("blockquote", "i am a quote")]
+        children2 = [LeafNode("li", "i am a list entry"), LeafNode("li", "me too"), LeafNode("li", "dito")]
+        parent_node2 = ParentNode("span", [ParentNode("div", children1)])
+        parent_node3 = ParentNode("ol", children2)
+        parent_node4 = ParentNode("p", [parent_node2, parent_node3])
+        self.assertEqual(
+            parent_node4.to_html(),
+            "<p><span><div>"
+            "<b>i am bold</b><i>i am italic</i><blockquote>i am a quote</blockquote>"
+            "</div></span>"
+            "<ol>"
+            "<li>i am a list entry</li>"
+            "<li>me too</li>"
+            "<li>dito</li>" 
+            "</ol></p>",
+        )
+
+    def test_parent_has_no_children(self):
+        def _helper():
+            ParentNode("p", []).to_html()
+        self.assertRaises(ValueError, _helper)
+    
+    def test_parent_has_no_tag(self):
+        def _helper():
+            ParentNode("", [LeafNode("b", "i am bold")]).to_html()
+        self.assertRaises(ValueError, _helper)
 
 
 
